a28659dc911851a7141f60cf2bed82b7faa8c331
Dmytro Honchar <dima@webart.work>
2026-03-24 17:59:01 +0200

    feat: update gallery component with new layout and images

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -7,4 +7,13 @@ import { TranslateDirective } from 'wacom';
 	styleUrl: './gallery.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GalleryComponent {}
+export class GalleryComponent {
+	protected readonly photos = [
+		{ src: 'gallery/1.png', alt: 'Podilska Knaipa photo 1' },
+		{ src: 'gallery/2.png', alt: 'Podilska Knaipa photo 2' },
+		{ src: 'gallery/3.png', alt: 'Podilska Knaipa photo 3' },
+		{ src: 'gallery/4.png', alt: 'Podilska Knaipa photo 4' },
+		{ src: 'gallery/5.png', alt: 'Podilska Knaipa photo 5' },
+		{ src: 'gallery/6.png', alt: 'Podilska Knaipa photo 6' },
+	];
+}
